b4a81fff901006babfcd747bbd0095dac54d9698
Raka Daffa <raka.daffa2005@gmail.com>
2025-03-18 23:31:10 +0700

    fix: makefile n kernel entrypoints

--- src/kernel-entrypoint.s
+++ src/kernel-entrypoint.s
@@ -1,51 +1,48 @@
-global loader       ; the entry symbol for ELF
-global load_gdt     ; load GDT table
-extern kernel_setup ; kernel
+global loader
+global load_gdt
+global loop        ; Tambahkan agar bisa digunakan di mana saja
+extern kernel_setup
 
-KERNEL_STACK_SIZE equ 4096           ; size of stack in bytes
-MAGIC_NUMBER      equ 0x1BADB002     ; define the magic number constant
-FLAGS             equ 0x0            ; multiboot flags
-CHECKSUM          equ -MAGIC_NUMBER  ; calculate the checksum
-                                     ; (magic number + checksum + flags should equal 0)
+KERNEL_STACK_SIZE equ 4096
+MAGIC_NUMBER      equ 0x1BADB002
+FLAGS             equ 0x0
+CHECKSUM          equ -MAGIC_NUMBER
 
 section .bss
-align 4                              ; align at 4 bytes
-kernel_stack:                        ; label points to beginning of memory
-    resb KERNEL_STACK_SIZE           ; reserve stack for the kernel
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
 
-section .multiboot                   ; GNU GRUB Multiboot header
-align 4                              ; the code must be 4 byte aligned
-    dd MAGIC_NUMBER                  ; write the magic number to the machine code,
-    dd FLAGS                         ; the flags,
-    dd CHECKSUM                      ; and the checksum
+section .multiboot
+align 4
+    dd MAGIC_NUMBER
+    dd FLAGS
+    dd CHECKSUM
 
-
-section .text                                  ; start of the text (code) 
-loader:                                        ; the loader label (defined as entry point in linker script)
-    mov  esp, kernel_stack + KERNEL_STACK_SIZE ; setup stack register to proper location
+section .text
+loader:
+    mov  esp, kernel_stack + KERNEL_STACK_SIZE
     call kernel_setup
-.loop:
-    jmp .loop                                  ; loop forever
+loop:
+    jmp loop   ; Ubah .loop menjadi loop
 
-
-; More details: https://en.wikibooks.org/wiki/X86_Assembly/Protected_Mode
 load_gdt:
     cli
     mov  eax, [esp+4]
-    lgdt [eax] ; Load GDT from GDTDescriptor, eax at this line will point GDTR location
+    test eax, eax
+    jz   loop   ; Gunakan loop, bukan .loop
+    lgdt [eax]
 
-    ; Set bit-0 (Protection Enable bit-flag) in Control Register 0 (CR0)
-    ; This is optional, as usually GRUB already start with protected mode flag enabled
     mov  eax, cr0
     or   eax, 1
     mov  cr0, eax
 
-    ; Far jump to update cs register
-    ; Warning: Invalid GDT will raise exception in following instruction below
     jmp 0x8:flush_cs
 flush_cs:
-    mov ax, 10h ; Update all segment register
+    mov ax, 10h
     mov ss, ax
     mov ds, ax
     mov es, ax
-    ret
+    jmp loop   ; Gunakan loop, bukan .loop
+
+section .note.GNU-stack noalloc
