722025a0196be1648f0ceb21c6c1e8ba6852a8e0
wrdtlkhoir <13523001@mahasiswa.itb.ac.id>
2025-03-24 11:47:45 +0700

    feat: kernel.c

---
+++ src/kernel.c
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "header/cpu/gdt.h"
+#include "header/kernel-entrypoint.h"
+
+void kernel_setup(void) {
+    load_gdt(&_gdt_gdtr);
+    while (true);
+
+}
