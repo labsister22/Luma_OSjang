ba27bb42d6641f4703c440eec933b302bb590371
wrdtlkhoir <13523001@mahasiswa.itb.ac.id>
2025-04-24 02:24:15 +0700

    feat: add driver disk

--- src/kernel.c
+++ src/kernel.c
@@ -7,6 +7,7 @@
 #include "header/cpu/idt.h"
 #include "header/cpu/interrupt.h"
 #include "header/driver/keyboard.h"
+#include "header/driver/disk.h"
 
 // void kernel_setup(void) {
 //     framebuffer_write(0, 0, 'L', 0xF, 0x0);
@@ -18,22 +19,23 @@
 //     framebuffer_write(0, 6, 'S', 0xF, 0x0);
 // }
 
-#include "header/text/framebuffer.h"
-#include "header/cpu/interrupt.h"
-#include "header/cpu/idt.h"
-#include "header/driver/keyboard.h"
-
 void kernel_setup(void)
 {
   load_gdt(&_gdt_gdtr);
   pic_remap();
   initialize_idt();
   activate_keyboard_interrupt();
+  asm volatile("sti");
   framebuffer_clear();
   framebuffer_set_cursor(0, 0);
 
   int row = 0, col = 0;
   keyboard_state_activate();
+  struct BlockBuffer b;
+  framebuffer_write(0, 0, 'L', 0xF, 0x0);
+  for (int i = 0; i < 512; i++) {b.buf[i] = i % 16;}
+  write_blocks(&b, 17, 1);
+  framebuffer_write(0, 0, 'L', 0xF, 0x0);
   while (true)
   {
     char c;
@@ -54,4 +56,16 @@ void kernel_setup(void)
     }
   }
 }
+// void kernel_setup(void) {
+//   load_gdt(&_gdt_gdtr);
+//   pic_remap();
+//   activate_keyboard_interrupt();
+//   initialize_idt();
+//   framebuffer_clear();
+//   framebuffer_set_cursor(0, 0);
 
+//   struct BlockBuffer b;
+//   for (int i = 0; i < 512; i++) {b.buf[i] = i % 16;}
+//   write_blocks(&b, 17, 1);
+//   while (true);
+// }
