012c166055cb0f106c22e1883dae93243ec1f8d8
Raka Daffa <raka.daffa2005@gmail.com>
2025-05-26 23:27:13 +0700

    feat: add 4.2

--- src/interrupt.c
+++ src/interrupt.c
@@ -60,7 +60,7 @@ void pic_remap(void)
 
 void main_interrupt_handler(struct InterruptFrame frame)
 {
-  
+
   uint32_t int_num = frame.int_number;
 
   switch (int_num)
@@ -77,24 +77,26 @@ void main_interrupt_handler(struct InterruptFrame frame)
     syscall(frame);
     break;
   case 0x0E: // Page Fault
-    {
-        uint32_t cr2;
-        __asm__ volatile("mov %%cr2, %0" : "=r"(cr2));
-        framebuffer_write(5, 0, 'F', 0xF, 0x0); // F for Fault
-        framebuffer_write(6, 0, ((cr2 >> 24) & 0xF) + '0', 0xF, 0x0);
-        framebuffer_write(7, 0, ((cr2 >> 20) & 0xF) + '0', 0xF, 0x0);
-        framebuffer_write(8, 0, ((cr2 >> 16) & 0xF) + '0', 0xF, 0x0);
-        framebuffer_write(9, 0, ((cr2 >> 12) & 0xF) + '0', 0xF, 0x0);
-        framebuffer_write(10, 0, ((cr2 >> 8) & 0xF) + '0', 0xF, 0x0);
-        framebuffer_write(11, 0, ((cr2 >> 4) & 0xF) + '0', 0xF, 0x0);
-        framebuffer_write(12, 0, (cr2 & 0xF) + '0', 0xF, 0x0);
-        // Print cr2, eip, error code, dsb
-        while(1);
-    }
-    break;
-  case 0x0D: // General Protection Fault
+  {
+    uint32_t cr2;
+    __asm__ volatile("mov %%cr2, %0" : "=r"(cr2));
+    framebuffer_write(5, 0, 'F', 0xF, 0x0); // F for Fault
+    framebuffer_write(6, 0, ((cr2 >> 24) & 0xF) + '0', 0xF, 0x0);
+    framebuffer_write(7, 0, ((cr2 >> 20) & 0xF) + '0', 0xF, 0x0);
+    framebuffer_write(8, 0, ((cr2 >> 16) & 0xF) + '0', 0xF, 0x0);
+    framebuffer_write(9, 0, ((cr2 >> 12) & 0xF) + '0', 0xF, 0x0);
+    framebuffer_write(10, 0, ((cr2 >> 8) & 0xF) + '0', 0xF, 0x0);
+    framebuffer_write(11, 0, ((cr2 >> 4) & 0xF) + '0', 0xF, 0x0);
+    framebuffer_write(12, 0, (cr2 & 0xF) + '0', 0xF, 0x0);
+    // Print cr2, eip, error code, dsb
+    while (1)
+      ;
+  }
+  break;
+  case 0x0D:                                // General Protection Fault
     framebuffer_write(6, 0, 'G', 0xF, 0x0); // G for GP Fault
-    while(1);
+    while (1)
+      ;
     break;
 
   default:
@@ -113,10 +115,11 @@ void activate_keyboard_interrupt(void)
   out(PIC1_DATA, in(PIC1_DATA) & ~(1 << IRQ_KEYBOARD));
 }
 
-struct rtc_time {
-    uint8_t hour;
-    uint8_t minute;
-    uint8_t second;
+struct rtc_time
+{
+  uint8_t hour;
+  uint8_t minute;
+  uint8_t second;
 };
 
 struct Time get_cmos_time();
@@ -136,41 +139,47 @@ void syscall(struct InterruptFrame frame)
 
   case 5: // SYS_PUTCHAR - Print single character
     framebuffer_write(
-        frame.cpu.general.edx,     // row
-        frame.cpu.general.ecx,     // col
+        frame.cpu.general.edx,            // row
+        frame.cpu.general.ecx,            // col
         *((char *)frame.cpu.general.ebx), // character
-        0x0F,                      // color (white on black)
-        0x00);                     // bg_color
+        0x0F,                             // color (white on black)
+        0x00);                            // bg_color
     break;
 
   case 6: // SYS_PUTS - Print string
-    {
-      char *str = (char *)frame.cpu.general.ebx;
-      uint8_t col = frame.cpu.general.edx;
-      uint8_t row = frame.cpu.general.ecx;
-      
-      // Simple puts implementation
-      uint32_t i = 0;
-      uint8_t current_col = col;
-      uint8_t current_row = row;
-      while (str[i] != '\0' && i < 1000) { // Safety limit
-        if (str[i] == '\n') {
-          current_row++;
-          current_col = 0;
-        } else {
-          framebuffer_write(current_row, current_col, str[i], 0x0F, 0x00);
-          current_col++;
-          if (current_col >= 80) {
-            current_col = 0;
-            current_row++;
-          }
-        }
-        i++;
-        if (current_row >= 25) break; // Screen height limit
+  {
+    char *str = (char *)frame.cpu.general.ebx;
+    uint8_t col = frame.cpu.general.edx;
+    uint8_t row = frame.cpu.general.ecx;
+
+    // Simple puts implementation
+    uint32_t i = 0;
+    uint8_t current_col = col;
+    uint8_t current_row = row;
+    while (str[i] != '\0' && i < 1000)
+    { // Safety limit
+      if (str[i] == '\n')
+      {
+        current_row++;
+        current_col = 0;
       }
-      framebuffer_set_cursor(current_row, current_col); // Update cursor position
+      else
+      {
+        framebuffer_write(current_row, current_col, str[i], 0x0F, 0x00);
+        current_col++;
+        if (current_col >= 80)
+        {
+          current_col = 0;
+          current_row++;
+        }
+      }
+      i++;
+      if (current_row >= 25)
+        break; // Screen height limit
     }
-    break;
+    framebuffer_set_cursor(current_row, current_col); // Update cursor position
+  }
+  break;
 
   case 7: // SYS_KEYBOARD_ACTIVATE - Activate keyboard
     keyboard_state_activate();
@@ -183,23 +192,23 @@ void syscall(struct InterruptFrame frame)
 
   case 9: // SYS_SET_CURSOR - Set cursor position (new)
     framebuffer_set_cursor(
-        frame.cpu.general.ecx,     // row
-        frame.cpu.general.ebx);    // col
+        frame.cpu.general.ecx,  // row
+        frame.cpu.general.ebx); // col
     break;
   case 10:
-      {
-          struct Time t = get_cmos_time();
-          struct Time* out = (struct Time*) frame.cpu.general.ebx;
-          out->hour = t.hour;
-          out->minute = t.minute;
-          out->second = t.second;
-      }
-      break;
-  // case 10: // SYS_GET_CURSOR - Get cursor position (new)
-  //   // You'll need to implement get_cursor functions in framebuffer
-  //   // For now, just return 0,0
-  //   *((uint8_t *)frame.cpu.general.ebx) = 0; // col
-  //   *((uint8_t *)frame.cpu.general.ecx) = 0; // row
+  {
+    struct Time t = get_cmos_time();
+    struct Time *out = (struct Time *)frame.cpu.general.ebx;
+    out->hour = t.hour;
+    out->minute = t.minute;
+    out->second = t.second;
+  }
+  break;
+    // case 10: // SYS_GET_CURSOR - Get cursor position (new)
+    //   // You'll need to implement get_cursor functions in framebuffer
+    //   // For now, just return 0,0
+    //   *((uint8_t *)frame.cpu.general.ebx) = 0; // col
+    //   *((uint8_t *)frame.cpu.general.ecx) = 0; // row
     // break;
 
   default:
@@ -212,4 +221,17 @@ void isr_handler(struct InterruptFrame frame)
 {
   // Simple handler - just return
   (void)frame; // Suppress unused parameter warning
+}
+
+void activate_timer_interrupt(void)
+{
+  __asm__ volatile("cli");
+  // Setup how often PIT fire
+  uint32_t pit_timer_counter_to_fire = PIT_TIMER_COUNTER;
+  out(PIT_COMMAND_REGISTER_PIO, PIT_COMMAND_VALUE);
+  out(PIT_CHANNEL_0_DATA_PIO, (uint8_t)(pit_timer_counter_to_fire & 0xFF));
+  out(PIT_CHANNEL_0_DATA_PIO, (uint8_t)((pit_timer_counter_to_fire >> 8) & 0xFF));
+
+  // Activate the interrupt
+  out(PIC1_DATA, in(PIC1_DATA) & ~(1 << IRQ_TIMER));
 }
